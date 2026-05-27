db3d0c9f20b5d7bb56cb24a56a5500bf7c40fa6f
Marsuzen19 <fernanflo.hptmail@gmail.com>
2026-05-27 14:03:37 -0500

    arreglado del vercel en blanco

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portafolio-Rios/", 
+    base: process.env.VERCEL
+    ? "/" // 👉 Vercel
+    : "/Portafolio-Rios/", // 👉 GitHub Pages
   plugins: [react()],
 })
